e408c8d58e4e653580f257677ee2a7f8a89bcf2e
sagarboyal <sboyal12@gmail.com>
2026-09-03 23:26:23 +0530

    config: setup postgres

--- backend/src/main/java/com/ps/backend/BackendApplication.java
+++ backend/src/main/java/com/ps/backend/BackendApplication.java
@@ -3,10 +3,13 @@ package com.ps.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
